a1120e20c497aa3c2d1cb201d40de4262a94ac3d
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-20 13:45:50 +0000

    Update arrivals.js

--- webnfc/arrivals.js
+++ webnfc/arrivals.js
@@ -1,3 +1,35 @@
+var OutputHelper = {
+  log: function() {
+    var line = Array.prototype.slice.call(arguments).map(function(argument) {
+      return typeof argument === 'string' ? argument : JSON.stringify(argument);
+    }).join(' ');
+
+    document.querySelector('#log').textContent += line + '\n';
+  },
+
+  clearLog: function() {
+    document.querySelector('#log').textContent = '';
+  },
+
+  setStatus: function(status) {
+    document.querySelector('#status').textContent = status;
+  },
+
+  setContent: function(newContent) {
+    var content = document.querySelector('#content');
+    while(content.hasChildNodes()) {
+      content.removeChild(content.lastChild);
+    }
+    content.appendChild(newContent);
+  }
+};
+
+log = OutputHelper.log;
+
+if (!("NDEFReader" in window))
+  OutputHelper.setStatus("Web NFC is not available. Use Chrome on Android.");
+</script>
+
 async function startScanning() {
   try {
     const nfcPermissionStatus = await navigator.permissions.query({ name: "nfc" });
