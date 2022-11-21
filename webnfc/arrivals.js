01a6a0c78648b07e0df70a0a31ad4f5e2c7412ac
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-21 16:29:24 +0000

    Update arrivals.js

--- webnfc/arrivals.js
+++ webnfc/arrivals.js
@@ -7,6 +7,8 @@ async function startScanning() {
     $('#scan_btn').text("NFC Scan Active...");
     $('#scan_btn').removeClass('btn-outline-light');
     $('#scan_btn').addClass('btn-success disabled');
+    if ($('#get_btn').hasClass('d-none') == false) $('#get_btn').addClass('d-none');
+    if ($('#stop_btn').hasClass('d-none') == false) $('#stop_btn').addClass('d-none');
 
     ndef.addEventListener("readingerror", () => {
       $('#arrivals_data').text("Argh! Cannot read data from the NFC tag. Try another one?");
