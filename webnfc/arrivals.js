b70eb19b2d3b08f88b57706f7b5e3bb86fb6e695
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-21 12:59:17 +0000

    Update arrivals.js

--- webnfc/arrivals.js
+++ webnfc/arrivals.js
@@ -4,29 +4,31 @@ async function startScanning() {
   try {
     const ndef = new NDEFReader();
     await ndef.scan();
-    $('#scanning_btn').text("NFC Scan Active...");
-    $('#scanning_btn').removeClass('btn-outline-light');
-    $('#scanning_btn').addClass('btn-success');
-    $('#arrivals_data').text("> Scan started.");
+    $('#scan_btn').text("NFC Scan Active...");
+    $('#scan_btn').removeClass('btn-outline-light');
+    $('#scan_btn').addClass('btn-success');
 
     ndef.addEventListener("readingerror", () => {
       $('#arrivals_data').text("Argh! Cannot read data from the NFC tag. Try another one?");
     });
 
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      $('#arrivals_data').append(`\n> Serial Number: ${serialNumber}`);
-      $('#arrivals_data').append(`\n> Records: (${message.records.length})`);
+      $('#arrivals_data').append(`> Serial Number: ${serialNumber}`);
+      $('#arrivals_data').append(`> Records: (${message.records.length})`);
       
       if (message.records.length > 0 && message.records[0].recordType != "empty") {
         const decoder = new TextDecoder();
         for (const record of message.records) {
-          $('#arrivals_data').append(`\n> Record Type: (${record.recordType})`);
+          $('#arrivals_data').append(`> Record Type: (${record.recordType})`);
+          $('#arrivals_data').append(`> Data: (${decoder.decode(record.data)})`);
           switch (record.recordType) {
             case "text":
               break;
             case "url":
               break;
             case "mime":
+              break;
+            case "smart-poster":
               break;
             default:
           }
