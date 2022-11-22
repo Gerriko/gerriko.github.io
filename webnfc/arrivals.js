f9df6aa89aa21a79eeccbc30be1d151baf69872d
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-22 13:25:05 +0000

    Update arrivals.js

--- webnfc/arrivals.js
+++ webnfc/arrivals.js
@@ -28,30 +28,32 @@ async function startScanning() {
         for (const record of message.records) {
           $('#arrivals_data').append(`<br/>NDEF Record Type: (${record.recordType})`);
           $('#arrivals_data').append(`<br/>NDEF Data: (${decoder.decode(record.data)})`);
+          $('#bus_btn').hide().addClass('d-none');
+          $('#train_btn').hide().addClass('d-none');
           switch (record.recordType) {
             case "text":
               const textDecoder = new TextDecoder(record.encoding);
               $('#arrivals_data').append(`<br/>Text: ${textDecoder.decode(record.data)} (${record.lang})`);
-              $('#get_btn').hide().addClass('d-none');
               break;
             case "url":
-              $('#get_btn').hide().addClass('d-none');
               break;
             case "mime":
-              $('#get_btn').hide().addClass('d-none');
               break;
             case "smart-poster":
               URLfind = false;
-              TXTfind = false;
+              TXTfind = 0;
               for (const sprecord of record.toRecords()) {
                 const spData = decoder.decode(sprecord.data);
                 $('#arrivals_data').append(`<br/>- SP Type: ${sprecord.recordType} | Data: ${spData}`);
                 if (sprecord.recordType == "url" && spData.includes("geo:53.")) URLfind = true;
-                else if (sprecord.recordType == "text" && spData.includes(", stop")) TXTfind = true;
+                else if (sprecord.recordType == "text" && spData.includes(", stop")) TXTfind = 1;
+                else if (sprecord.recordType == "text" && spData.includes(", StationCode")) TXTfind = 2;
               }
               
-              if (URLfind == true && TXTfind == true)
-                $('#get_btn').hide().removeClass('d-none').fadeIn();                
+              if (URLfind == true && TXTfind == 1)
+                $('#bus_btn').hide().removeClass('d-none').fadeIn();                
+              if (URLfind == true && TXTfind == 2)
+                $('#train_btn').hide().removeClass('d-none').fadeIn();                
                 
               break;
             default:
@@ -68,7 +70,8 @@ async function startScanning() {
 async function stopScan() {
   await AbortCtrlr.abort();
   $('#arrivals_data').text("");
-  $('#get_btn').hide().addClass('d-none');
+  $('#bus_btn').hide().addClass('d-none');
+  $('#train_btn').hide().addClass('d-none');
   $('#stop_btn').hide().addClass('d-none');
   $('#arrivals_canvas').addClass('d-none').fadeOut();
   $('#scan_btn').removeClass('btn-warning disabled');
