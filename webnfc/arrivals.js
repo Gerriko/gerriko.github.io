a07841fe7a6e98d9fc85786c7366c1f58e6c62ce
Colin G(IO) <gerriko.iot@gmail.com>
2022-11-28 12:11:31 +0000

    Update arrivals.js

--- webnfc/arrivals.js
+++ webnfc/arrivals.js
@@ -79,6 +79,7 @@ async function startScanning() {
 			  			if (record.mediaType === "application/json") {
 								$('#arrivals_data').append(`<br/>Mime JSON: ${JSON.parse(decoder.decode(record.data))}`);
       				} else {
+                const textDecoder = new TextDecoder(record.encoding);
 								$('#arrivals_data').append(`<br/>Text: ${textDecoder.decode(record.data)}`);
         				console.log(`MIME Media not handled`);
       				}
